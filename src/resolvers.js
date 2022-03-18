ae446fac234b0cd425544c0401e4dcbfa62bbaa8
Bruno Santos <bruno.s.l.santos@gmail.com>
2022-03-18 11:17:10 +0000

    change Schema of Track and Module to new better name of length to durationInSeconds

--- src/resolvers.js
+++ src/resolvers.js
@@ -40,10 +40,14 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    /* Track.author and Track.modules resolvers */
+    durationInSeconds: ({ length }) => length,
+  },
+  Module: {
+    durationInSeconds: ({ length }) => length,
   },
 };
 
